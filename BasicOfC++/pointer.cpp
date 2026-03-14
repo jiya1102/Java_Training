7fbcea235c97db74f32ad2807060bab46b990fd8
jiya1102 <sonijiya752@gmail.com>
2026-03-14 10:49:37 +0530

    updated commit

---
+++ BasicOfC++/pointer.cpp
@@ -0,0 +1,8 @@
+# include <stdio.h>
+main()
+{
+  int i;
+  int * pi=&i;
+  scanf( "%d", pi) ;
+  printf ("%d \n", i+5) ;
+}
---
+++ BasicsOfJava/bit.java
@@ -0,0 +1,20 @@
+package BasicsOfJava;
+
+public class bit {
+    
+
+    public static void main (String [] args) 
+    {  
+        int x = 0; 
+        int y = 0 ;
+        for (int z = 0; z < 5; z++)
+        { 
+            if((++x >2)||(++y  > 2)) 
+            {
+                x++;
+            }
+        }
+    System.out.println (x+ " " + y); 
+    }
+
+}
