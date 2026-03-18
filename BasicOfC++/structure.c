444a83845506309eb562559bafbf072f2fd415f0
jiya1102 <sonijiya752@gmail.com>
2026-03-18 08:45:04 +0530

    updated commit

---
+++ BasicOfC++/structure.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+typedef struct {
+    char *a;
+    char *b;
+    } t;
+void f1 (t s);
+void f2 (t *p);
+int main()
+{
+    static t s = {"A", "B"};
+    printf ("%s %s\n", s.a, s.b);
+    f1(s);
+    printf ("%s %s\n", s.a, s.b);
+    f2(&s);
+    printf ("%s %s\n", s.a, s.b);
+}
+void f1 (t s)
+{
+    s.a = "U";
+    s.b = "V";
+    printf ("%s %s\n", s.a, s.b);
+    return;
+}
+void f2(t *p)
+{
+    p -> a  = "V";
+    p -> b = "W";
+    printf("%s %s\n", p -> a, p -> b);
+    return;
+}
