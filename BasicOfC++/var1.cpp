2448716a5bb1a0eecc7c381115162f9dead12100
jiya1102 <sonijiya752@gmail.com>
2026-03-19 10:18:28 +0530

    updated commit

---
+++ BasicOfC++/var1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =(20,10,20);
+    cout<<a;
+
+    
+}
---
+++ BasicOfC++/var2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x=10;
+     x=printf("made%d",x);
+    // x= printf("hello");
+    printf("%d\n",x);
+
+    x=x+2;
+    printf("%d\n",x);
+}
