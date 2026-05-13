e110a91cc1a7365ec7f62adcf9f1934f0e2d5d74
jiya1102 <sonijiya752@gmail.com>
2026-05-13 09:47:09 +0530

    updated commit

---
+++ PATTERN/Butterfly.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    // upper part
+    for(int i=1; i<=n; i++) {
+
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+
+        for(int j=1; j<=2*(n-i); j++) {
+            cout << " ";
+        }
+
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // lower part
+    for(int i=n; i>=1; i--) {
+
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+
+        for(int j=1; j<=2*(n-i); j++) {
+            cout << " ";
+        }
+
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
