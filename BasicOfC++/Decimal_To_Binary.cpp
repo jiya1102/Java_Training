cfe06dd4a28c00873c6a53f607351ceb98eae740
jiya1102 <sonijiya752@gmail.com>
2026-05-26 09:14:59 +0530

    updated commit

---
+++ BasicOfC++/Decimal_To_Binary.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, binary[32], i = 0;
+
+    cout << "Enter decimal number: ";
+    cin >> n;
+
+    while(n > 0) {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    cout << "Binary = ";
+
+    for(int j = i - 1; j >= 0; j--) {
+        cout << binary[j];
+    }
+
+    return 0;
+}
